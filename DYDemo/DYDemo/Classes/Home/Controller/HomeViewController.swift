8a50e8a39b065a707932112a6c495970793cff29
pingping_float@163.com <pingping_float@163.com>
2017-08-02 13:08:47 +0800

    创建公共类：Common.swift

--- DYDemo/DYDemo/Classes/Home/Controller/HomeViewController.swift
+++ DYDemo/DYDemo/Classes/Home/Controller/HomeViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
+private let pTitleViewH : CGFloat = 40
+
 class HomeViewController: UIViewController {
+    //MARK:- 懒加载
+    private lazy var pageTitleView:PageTitleView = {
+        let titleFrame = CGRect(x: 0, y: pStatusBarH+pNavigationBarH, width: pScreenWidth, height: pTitleViewH)
+        let titles = ["推荐","游戏","娱乐","趣玩"]
+        let titleView = PageTitleView(frame: titleFrame, titles: titles)
+        return titleView
+    }()
     
+    
+    
+    //MARK:- 系统回调函数
     override func viewDidLoad() {
         super.viewDidLoad()
         /// 设置ui界面
@@ -18,7 +30,8 @@ class HomeViewController: UIViewController {
     
 }
 
-// Mark:- 设置ui界面
+
+//MARK:- 设置ui界面
 private extension HomeViewController {
     
     func setupUI() {
@@ -34,12 +47,11 @@ private extension HomeViewController {
         
         /// 2.设置右侧items
         let size = CGSize(width: 40, height: 40)
-//        let qrcodeItem = UIBarButtonItem.createItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: size)
+        //        let qrcodeItem = UIBarButtonItem.createItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: size)
         
         let historyItem = UIBarButtonItem(imageName: "image_my_history", highImageName: "Image_my_history_click", size: size)
         let searchItem = UIBarButtonItem(imageName: "btn_search", highImageName: "btn_search_clicked", size: size)
         let qrcodeItem = UIBarButtonItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: size)
-        
         navigationItem.rightBarButtonItems = [historyItem,searchItem,qrcodeItem]
         
     }
---
+++ DYDemo/DYDemo/Classes/Tools/Common.swift
@@ -0,0 +1,17 @@
+//
+//  Common.swift
+//  DYDemo
+//
+//  Created by ZYP-MAC on 2017/8/2.
+//  Copyright © 2017年 ZYP-MAC. All rights reserved.
+//
+
+import UIKit
+/// 电池栏高度
+let pStatusBarH : CGFloat = 20
+/// 导航条高度
+let pNavigationBarH : CGFloat = 44
+/// 屏幕宽度
+let pScreenWidth : CGFloat = UIScreen.main.bounds.width
+/// 屏幕高度
+let pScreenHeight : CGFloat = UIScreen.main.bounds.height
